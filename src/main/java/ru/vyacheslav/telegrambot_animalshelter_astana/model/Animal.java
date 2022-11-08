7b9fab3937a4baa496b34fa5e31b95c6bcd8a063
asyof <asyofka@mail.ru>
2022-11-08 18:28:19 +0300

    Добавил в модели поля, геттеры, сеттеры, equals, hashCode, toString

--- src/main/java/ru/vyacheslav/telegrambot_animalshelter_astana/model/Animal.java
+++ src/main/java/ru/vyacheslav/telegrambot_animalshelter_astana/model/Animal.java
@@ -1,12 +1,70 @@
 package ru.vyacheslav.telegrambot_animalshelter_astana.model;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+
+import javax.persistence.*;
+import java.util.Objects;
+
 
 @Entity
+@Table(name = "Animal")
 public class Animal {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String name;
+    private Long personId;
 
+    public Animal() {
+    }
+
+    public Animal(Long id, String name, Long personId) {
+        this.id = id;
+        this.name = name;
+        this.personId = personId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(Long personId) {
+        this.personId = personId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return Objects.equals(id, animal.id) && Objects.equals(name, animal.name) && Objects.equals(personId, animal.personId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, personId);
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", personId=" + personId +
+                '}';
+    }
 }
--- src/main/java/ru/vyacheslav/telegrambot_animalshelter_astana/model/Report.java
+++ src/main/java/ru/vyacheslav/telegrambot_animalshelter_astana/model/Report.java
@@ -1,13 +1,102 @@
 package ru.vyacheslav.telegrambot_animalshelter_astana.model;
 
-import javax.persistence.Entity;
-import javax.annotation.processing.Generated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
+@Table(name = "Report")
 public class Report {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String photoPath;
+    private Integer photoSize;
+    private String description;
+    private String reportDate;
+    private Long personId;
+
+    public Report() {
+    }
+
+    public Report(Long id, String photoPath, Integer photoSize, String description, String reportDate, Long personId) {
+        this.id = id;
+        this.photoPath = photoPath;
+        this.photoSize = photoSize;
+        this.description = description;
+        this.reportDate = reportDate;
+        this.personId = personId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPhotoPath() {
+        return photoPath;
+    }
+
+    public void setPhotoPath(String photoPath) {
+        this.photoPath = photoPath;
+    }
+
+    public Integer getPhotoSize() {
+        return photoSize;
+    }
+
+    public void setPhotoSize(Integer photoSize) {
+        this.photoSize = photoSize;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getReportDate() {
+        return reportDate;
+    }
+
+    public void setReportDate(String reportDate) {
+        this.reportDate = reportDate;
+    }
+
+    public Long getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(Long personId) {
+        this.personId = personId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Report report = (Report) o;
+        return Objects.equals(id, report.id) && Objects.equals(photoPath, report.photoPath) && Objects.equals(photoSize, report.photoSize) && Objects.equals(description, report.description) && Objects.equals(reportDate, report.reportDate) && Objects.equals(personId, report.personId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, photoPath, photoSize, description, reportDate, personId);
+    }
+
+    @Override
+    public String toString() {
+        return "Report{" +
+                "id=" + id +
+                ", photoPath='" + photoPath + '\'' +
+                ", photoSize=" + photoSize +
+                ", description='" + description + '\'' +
+                ", reportDate='" + reportDate + '\'' +
+                ", personId=" + personId +
+                '}';
+    }
 }

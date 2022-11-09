b0f6df9f0d7da8a0b031235b5ae1229bb41ccde8
Lexxkit <lexxkit@gmail.com>
2022-11-09 19:06:56 +0000

    добавил классы TelegramBotConfiguration.java и TelegramBotUpdatesListener необходимые для работы бота; добавил обработку команды /start; добавил пакет constants и класс TelegramBotConstants.java для хранения типовых сообщений-ответов от бота;

---
+++ src/main/java/ru/vyacheslav/telegrambot_animalshelter_astana/constants/TelegramBotConstants.java
@@ -0,0 +1,6 @@
+package ru.vyacheslav.telegrambot_animalshelter_astana.constants;
+
+public class TelegramBotConstants {
+    public static final String START_CMD = "/start";
+    public static final String GREETING_MSG = "Приветствую тебя в боте-пощнике питомника для собак!";
+}

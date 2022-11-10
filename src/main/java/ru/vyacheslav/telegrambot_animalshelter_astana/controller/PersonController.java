c2628404eb8c44909403eccf2cc92b67570d8479
Lexxkit <lexxkit@gmail.com>
2022-11-10 18:03:46 +0000

    add CRUD methods and endpoints to PersonService & PersonController;

--- src/main/java/ru/vyacheslav/telegrambot_animalshelter_astana/controller/PersonController.java
+++ src/main/java/ru/vyacheslav/telegrambot_animalshelter_astana/controller/PersonController.java
@@ -1,19 +1,18 @@
 package ru.vyacheslav.telegrambot_animalshelter_astana.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.vyacheslav.telegrambot_animalshelter_astana.model.Person;
 import ru.vyacheslav.telegrambot_animalshelter_astana.service.PersonService;
 
 import java.util.Collection;
+import java.util.Optional;
 
 /**
  * @author Oleg Alekseenko
  */
 @RestController
-@RequestMapping("/people")
+@RequestMapping(path = "/people")
 public class PersonController {
     private final PersonService personService;
 
@@ -24,5 +23,34 @@ public class PersonController {
     @GetMapping
     public ResponseEntity<Collection<Person>> getAllPeople() {
         return ResponseEntity.ok(personService.findAll());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Person> getPersonById(@PathVariable long id) {
+        return ResponseEntity.ok(personService.findPerson(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Person> createPerson(@RequestBody Person person) {
+        Person createdPerson = personService.createPerson(person);
+        if (createdPerson == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(createdPerson);
+    }
+
+    @PutMapping
+    public ResponseEntity<Person> updatePerson(@RequestBody Person person) {
+        Person updatedPerson = personService.updatePerson(person);
+        if (updatedPerson == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(updatedPerson);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> deletePerson(@PathVariable long id) {
+        personService.deletePerson(id);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/ru/vyacheslav/telegrambot_animalshelter_astana/service/PersonService.java
+++ src/main/java/ru/vyacheslav/telegrambot_animalshelter_astana/service/PersonService.java
@@ -2,12 +2,15 @@ package ru.vyacheslav.telegrambot_animalshelter_astana.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
+import ru.vyacheslav.telegrambot_animalshelter_astana.exceptions.PersonNotFoundException;
 import ru.vyacheslav.telegrambot_animalshelter_astana.model.Person;
 import ru.vyacheslav.telegrambot_animalshelter_astana.repository.PersonRepository;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Optional;
 
 /**
  * @author Oleg Alekseenko
@@ -23,8 +26,40 @@ public class PersonService {
     }
 
 
+    /**
+     * Finds all entries in DB.
+     *
+     * @return unmodifiable collection
+     */
     public Collection<Person> findAll() {
         logger.info("Was invoked method to get all people");
         return Collections.unmodifiableCollection(personRepository.findAll());
+    }
+
+    /**
+     * Finds record for person in DB.
+     *
+     * @param id identification number for person
+     * @return person model
+     * @throws PersonNotFoundException if no entry was found in DB
+     */
+    public Person findPerson(long id) {
+        logger.info("Was invoked method for find person by id: {}", id);
+        return personRepository.findById(id).orElseThrow(PersonNotFoundException::new);
+    }
+
+    public Person createPerson(Person person) {
+        logger.info("Was invoked method for create person");
+        return personRepository.save(person);
+    }
+
+    public Person updatePerson(Person person) {
+        logger.info("Was invoked method for update person");
+        return personRepository.save(person);
+    }
+
+    public void deletePerson(long id) {
+        logger.info("Was invoked method for delete person by id: {}", id);
+        personRepository.deleteById(id);
     }
 }
